36183841deb3fd85f3d69a31a184971f8905e7fb
ooyuo <doyounjeong0@gmail.com>
2022-08-22 05:39:57 +0900

    #02. Apply Global Style

--- portfolio/src/routes/Websites.tsx
+++ portfolio/src/routes/Websites.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+    color: ${(props) => props.theme.accentColor};
+`;
 
 function Websites() {
-    return <h1>Websites</h1>;
+    return <h1>Portfolio</h1>;
 }
 
 export default Websites;
